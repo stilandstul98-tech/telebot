88878430b42c04990e6bca457820b0474e839ca7
Stas <stilandstul98@gmail.com>
2026-03-10 12:57:12 +0200

    Add help echo handlers

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 from config import BOT_TOKEN
@@ -13,6 +13,21 @@ dp = Dispatcher()
 
 async def cmd_start(message: Message):
     await message.answer("Привет я твой первый бот")
+    
+@dp.message(Command("help"))
+
+async def cmd_help(message: Message):
+    await message.answer("Доступные команды: \n/start - начать\n/help - помощь")
+    
+@dp.message(F.text)
+
+async def echo(message: Message):
+    await message.answer(message.text)
+
+
+
+
+
     
 async def main():
     await dp.start_polling(bot)
